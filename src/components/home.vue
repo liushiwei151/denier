42acbee022ddf775f66be564c2650fe5d81f083c
DESKTOP-V2D67I1\ydm <478807601@qq.com>
2019-12-10 10:54:07 +0800

    初始文件

---
+++ src/components/home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class='home'>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+.home{
+  width: 750px;
+  height: 1334px;
+  background: url(../../static/homepagebg.jpg) no-repeat;
+  background-size: 100% 100%;
+}
+</style>
